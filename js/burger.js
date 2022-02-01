b7ff4d27d94f6801764cd98648146257554c7c45
jdbench <justin.d.bench@gmail.com>
2022-01-31 19:31:54 -0700

    fix

--- js/burger.js
+++ js/burger.js
@@ -1,3 +1,18 @@
+$(document).on('touchstart', function() {
+    detectTap = true; // Detects all touch events
+});
+$(document).on('touchmove', function() {
+    detectTap = false; // Excludes the scroll events from touch events
+});
+$(document).on('click touchend', function(event){
+    if (event.type == "click") detectTap = true;
+        if (detectTap){
+	        $('#nav-icon').click(function(){
+		        $(this).toggleClass('open');
+                $('.main-nav').toggleClass('flex');
+	        });
+        }
+});
 $(document).ready(function(){
 	$('#nav-icon').click(function(){
 		$(this).toggleClass('open');
