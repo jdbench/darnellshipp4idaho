d196f0d511b66beb5f29f75e3f1c8b2aff29e746
jdbench <justin.d.bench@gmail.com>
2022-01-31 22:18:10 -0700

    finished mobile view of home page

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,6 +2,6 @@ $( document ).ready(function() {
     $('.core-value-heading').click(function(){
       $(this).parent().children('div:nth-child(2)').toggleClass('expanded');  
       $(this).parent().children('div:nth-child(2)').toggleClass('value-info');
-      $(this).children('.core-value-heading').children('span').toggleClass('rotated-caret').toggleClass('caret');
+      $(this).children('span').toggleClass('rotated-caret');
     });
   });
