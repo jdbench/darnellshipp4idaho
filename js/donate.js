d099ba990f713f2ec32b515757c6bf1356e3bcc1
jdbench <justin.d.bench@gmail.com>
2022-03-18 19:18:27 -0600

    updated donation page

--- js/donate.js
+++ js/donate.js
@@ -6,4 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     createFooterYear();
     fontAwesome();
     burger();
+    let n={};
+    window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(e,m,d){n[m]=d});
+    n.email||n.phone_number?n.email?document.getElementById("iframeid").src=document.getElementById("iframeid").name+"?email="+n.email:n.phone_number?document.getElementById("iframeid").src=document.getElementById("iframeid").name+"?phone_number="+n.phone_number:document.getElementById("iframeid").src=document.getElementById("iframeid").name+"?email="+n.email+"&phone_number="+n.phone_number:document.getElementById("iframeid").src=document.getElementById("iframeid").name+"?id="+n.id+"&user_id="+n.user_id;
 });
